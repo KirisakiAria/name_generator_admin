fb37e132d78ac73c49ac2062c44d46e8884b8fd4
kirisakiaria <xiaoli350791904@hotmail.com>
2020-11-22 20:06:12 +0800

    feat: 开发vip功能

--- src/views/error/ErrorDetails.vue
+++ src/views/error/ErrorDetails.vue
@@ -1,18 +1,18 @@
 <template>
   <section class="details-page">
     <el-row>
-      <el-col :span="12">app版本：{{ item.appVersion }}</el-col>
-      <el-col :span="12">厂商：{{ item.brand }}</el-col>
+      <el-col :span="12">app版本：{{ selectedItem.appVersion }}</el-col>
+      <el-col :span="12">厂商：{{ selectedItem.brand }}</el-col>
     </el-row>
     <el-row>
-      <el-col :span="12">系统类型：{{ item.system }}</el-col>
-      <el-col :span="12">系统版本：{{ item.systemVersion }}</el-col>
+      <el-col :span="12">系统类型：{{ selectedItem.system }}</el-col>
+      <el-col :span="12">系统版本：{{ selectedItem.systemVersion }}</el-col>
     </el-row>
     <el-row>
-      <el-col :span="24">错误信息：{{ item.error }}</el-col>
+      <el-col :span="24">错误信息：{{ selectedItem.error }}</el-col>
     </el-row>
     <el-row>
-      <el-col :span="24">堆栈跟踪：{{ item.stackTrace }}</el-col>
+      <el-col :span="24">堆栈跟踪：{{ selectedItem.stackTrace }}</el-col>
     </el-row>
   </section>
 </template>
--- src/views/feedback/FeedbackDetails.vue
+++ src/views/feedback/FeedbackDetails.vue
@@ -1,15 +1,15 @@
 <template>
   <section class="details-page">
     <el-row>
-      <el-col :span="12">手机号：{{ item.tel }}</el-col>
-      <el-col :span="12">用户名：{{ item.username }}</el-col>
+      <el-col :span="12">手机号：{{ selectedItem.tel }}</el-col>
+      <el-col :span="12">用户名：{{ selectedItem.username }}</el-col>
     </el-row>
     <el-row>
-      <el-col :span="12">邮箱：{{ item.email }}</el-col>
-      <el-col :span="12">反馈时间：{{ time(item.date) }}</el-col>
+      <el-col :span="12">邮箱：{{ selectedItem.email }}</el-col>
+      <el-col :span="12">反馈时间：{{ time(selectedItem.date) }}</el-col>
     </el-row>
     <el-row>
-      <el-col :span="24">反馈信息：{{ item.content }}</el-col>
+      <el-col :span="24">反馈信息：{{ selectedItem.content }}</el-col>
     </el-row>
   </section>
 </template>
